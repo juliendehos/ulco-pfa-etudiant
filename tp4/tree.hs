9307b96f285b63614d61cf8f67453e7b454da5b0
Julien Dehos <dehos@univ-littoral.fr>
2020-04-17 11:52:38 +0200

    tp4

--- tp4/tree.hs
+++ tp4/tree.hs
@@ -1,7 +1,12 @@
 
-mytree = Node 7 (Node 2 Leaf Leaf)
+mytree1 :: Tree
+mytree1 = Node 7 (Node 2 Leaf Leaf)
                 (Node 37 (Node 13 Leaf Leaf)
                          (Node 42 Leaf Leaf))
 
-main = print mytree
+main = do
+    print mytree1
+    -- print mytree2
+    -- print $ sum mytree1
+    -- print $ maximum mytree1
 
