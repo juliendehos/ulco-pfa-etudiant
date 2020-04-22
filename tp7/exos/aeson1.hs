86256cb9bd0cd756e4dd0ea4b842d23359defc6d
Julien Dehos <dehos@univ-littoral.fr>
2020-04-22 20:27:28 +0200

    tp7 aeson

--- tp7/exos/aeson1.hs
+++ tp7/exos/aeson1.hs
@@ -3,13 +3,14 @@
 import qualified Data.Text as T
 
 data Person = Person
-    { firstname :: T.Text
-    , lastname  :: T.Text
-    , birthyear :: Int
+    { firstname    :: T.Text
+    , lastname     :: T.Text
+    , birthyear    :: T.Text
+    , speakenglish :: Bool
     } deriving (Show)
 
 main :: IO ()
 main = do
-    let res0 = Person "John" "Doe" 1970
+    let res0 = Person "John" "Doe" "1970" False
     print res0
 
