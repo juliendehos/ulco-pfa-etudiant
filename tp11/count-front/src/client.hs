f3fa2af8cb93b109407265540880be689c51c223
Julien Dehos <dehos@univ-littoral.fr>
2020-05-02 01:43:07 +0200

    tp11 count-front

---
+++ tp11/count-front/src/client.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Miso
+import Miso.String
+
+data Model = Model
+    { _val :: Int
+    } deriving (Eq)
+
+data Action
+    = ActionInc
+    | ActionDec
+    | ActionNone
+    deriving (Eq)
+
+main :: IO ()
+main = startApp App
+    { initialAction = ActionNone
+    , update        = updateModel
+    , view          = viewModel
+    , model         = Model 0
+    , subs          = []
+    , events        = defaultEvents
+    , mountPoint    = Nothing
+    }
+
+updateModel :: Action -> Model -> Effect Action Model
+updateModel ActionInc m = noEff m { _val = (_val m) + 1 }
+updateModel ActionDec m = noEff m { _val = (_val m) - 1 }
+updateModel ActionNone m = noEff m
+
+viewModel :: Model -> View Action
+viewModel m =
+    div_ []
+        [ h1_ [ ] [ text "count-front" ]
+        , p_ [] [ text ("val = " <> ms (_val m)) ]
+        , button_ [ onClick ActionInc ] [ text "+" ]
+        , button_ [ onClick ActionDec ] [ text "-" ]
+        ]
+
